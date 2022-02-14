a12e54768f8a656c6d975e69fee6111b4f86c1cc
Adam Taylor <afranklint16@gmail.com>
2022-02-14 13:05:22 -0700

    Chat utils

--- src/main/java/com/github/pocketkid2/spacecore/SpaceCorePlugin.java
+++ src/main/java/com/github/pocketkid2/spacecore/SpaceCorePlugin.java
@@ -1,11 +1,14 @@
 package com.github.pocketkid2.spacecore;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.pocketkid2.spacecore.commands.SpaceCoreCommand;
 
 public class SpaceCorePlugin extends JavaPlugin {
+
+	private static final ChatColor prefixColor = ChatColor.GRAY;
 
 	public static SpaceCorePlugin getInstance() {
 		return (SpaceCorePlugin) Bukkit.getPluginManager().getPlugin("SpaceCore");
@@ -21,5 +24,9 @@ public class SpaceCorePlugin extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		getLogger().info("Disabled!");
+	}
+
+	public String addPrefix(String message) {
+		return String.format(ChatColor.AQUA + "[" + prefixColor + "%s" + ChatColor.AQUA + "] %s", getDescription().getName(), message);
 	}
 }
