947da1a2f367fd1720cf40e17ba98a2d9663ddcf
Adam Taylor <afranklint16@gmail.com>
2022-02-14 13:05:40 -0700

    Implement list command

--- src/main/java/com/github/pocketkid2/spacecore/commands/ListWorldsSubCommand.java
+++ src/main/java/com/github/pocketkid2/spacecore/commands/ListWorldsSubCommand.java
@@ -2,11 +2,14 @@ package com.github.pocketkid2.spacecore.commands;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 
 import com.github.pocketkid2.spacecore.SpaceCorePlugin;
 import com.github.pocketkid2.spacecore.commands.SpaceCoreCommand.SpaceCoreSubCommand;
+import com.github.pocketkid2.spacecore.generation.SpaceChunkGenerator;
 
 public class ListWorldsSubCommand extends SpaceCoreSubCommand {
 
@@ -46,6 +49,11 @@ public class ListWorldsSubCommand extends SpaceCoreSubCommand {
 
 	@Override
 	public void execute(String label, String[] args, CommandSender sender) {
+		List<World> worlds = plugin.getServer().getWorlds().stream().filter(w -> w.getGenerator() instanceof SpaceChunkGenerator).collect(Collectors.toList());
+		sender.sendMessage(plugin.addPrefix(String.format("%d worlds loaded:", worlds.size())));
+		for (World world : worlds) {
+			sender.sendMessage(plugin.addPrefix(String.format("World name: %s, World type: %s", world.getName(), world.getGenerator().getClass().getName())));
+		}
 	}
 
 }
